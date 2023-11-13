0ce64fa5930c837e91d50548cf5f264155cde211
unknown <olyguy.aq@gmail.com>
2023-11-13 17:13:59 +0100

    add media query

--- src/main/java/com/cgpaCalculator/resources/CgpaCalculatorResource.java
+++ src/main/java/com/cgpaCalculator/resources/CgpaCalculatorResource.java
@@ -56,7 +56,45 @@ public class CgpaCalculatorResource {
                 "<head>\n" +
                 "    <meta charset=\"UTF-8\">\n" +
                 "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                "    <link rel=\"stylesheet\" href=\"/path/to/style.css\">\n" +
+                "    <style>\n" +
+                "        body {\n" +
+                "            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;\n" +
+                "            background-color: #f5f5f5;\n" +
+                "            margin: 0;\n" +
+                "            padding: 0;\n" +
+                "            display: flex;\n" +
+                "            justify-content: center;\n" +
+                "            align-items: center;\n" +
+                "            height: 100vh;\n" +
+                "        }\n" +
+                "\n" +
+                "        .result-container {\n" +
+                "            background-color: #fff;\n" +
+                "            border-radius: 8px;\n" +
+                "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" +
+                "            padding: 20px;\n" +
+                "            text-align: center;\n" +
+                "        }\n" +
+                "\n" +
+                "        h1 {\n" +
+                "            color: #333;\n" +
+                "        }\n" +
+                "\n" +
+                "        .result-box {\n" +
+                "            background-color: #dff0d8;\n" +
+                "            border: 1px solid #d6e9c6;\n" +
+                "            color: #3c763d;\n" +
+                "            padding: 15px;\n" +
+                "            border-radius: 4px;\n" +
+                "            margin-top: 20px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #cgpa-value {\n" +
+                "            font-size: 24px;\n" +
+                "            font-weight: bold;\n" +
+                "            color: #4caf50;\n" +
+                "        }\n" +
+                "    </style>\n" +
                 "    <title>CGPA Result</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
@@ -67,58 +105,105 @@ public class CgpaCalculatorResource {
                 "        </div>\n" +
                 "    </div>\n" +
                 "</body>\n" +
-                "</html>";
-    }
+                "</html>";    }
+// Render the HTML form
+private String renderForm() {
+    return "<!DOCTYPE html>\n" +
+            "<html lang=\"en\">\n" +
+            "<head>\n" +
+            "    <meta charset=\"UTF-8\">\n" +
+            "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+            "    <style>\n" +
+            "        /* Overall form styling */\n" +
+            "        .input-container {\n" +
+            "            background-color: #ffffff;\n" +
+            "            border-radius: 8px;\n" +
+            "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" +
+            "            padding: 20px;\n" +
+            "            width: 80%;\n" +
+            "            max-width: 400px;\n" +
+            "            text-align: center;\n" +
+            "            margin: 20px;\n" +
+            "        }\n" +
+            "\n" +
+            "        /* Label styling */\n" +
+            "        label {\n" +
+            "            display: block;\n" +
+            "            margin: 10px 0 5px;\n" +
+            "            font-weight: bold;\n" +
+            "            font-size: 16px;\n" +
+            "        }\n" +
+            "\n" +
+            "        /* Input styling */\n" +
+            "        input {\n" +
+            "            width: 100%;\n" +
+            "            padding: 8px;\n" +
+            "            margin-bottom: 15px;\n" +
+            "            box-sizing: border-box;\n" +
+            "            border: 1px solid #ccc;\n" +
+            "            border-radius: 4px;\n" +
+            "        }\n" +
+            "\n" +
+            "        /* Button styling */\n" +
+            "        button {\n" +
+            "            background-color: #4caf50;\n" +
+            "            color: white;\n" +
+            "            padding: 10px 20px;\n" +
+            "            border: none;\n" +
+            "            border-radius: 4px;\n" +
+            "            cursor: pointer;\n" +
+            "            font-size: 16px;\n" +
+            "        }\n" +
+            "\n" +
+            "        /* Media query for responsiveness */\n" +
+            "        @media (max-width: 768px) {\n" +
+            "            .input-container {\n" +
+            "                width: 90%;\n" +
+            "                max-width: none;\n" +
+            "            }\n" +
+            "        }\n" +
+            "    </style>\n" +
+            "    <title>CGPA Calculator</title>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "    <div class=\"input-container\">\n" +
+            "        <h1>Enter Your Scores and Units</h1>\n" +
+            "        <form action=\"/\" method=\"get\">\n" +
+            "            <label for=\"score1\">FSC 701:</label>\n" +
+            "            <input type=\"number\" id=\"score1\" name=\"score1\" required>\n" +
+            "            <label for=\"unit1\">Unit:</label>\n" +
+            "            <input type=\"number\" id=\"unit1\" name=\"unit1\" value=\"2\" required>\n" +
+            "            <label for=\"score2\">FSC 715:</label>\n" +
+            "            <input type=\"number\" id=\"score2\" name=\"score2\" required>\n" +
+            "            <label for=\"unit2\">Unit:</label>\n" +
+            "            <input type=\"number\" id=\"unit2\" name=\"unit2\" value=\"2\" required>\n" +
+            "            <label for=\"score3\">FSC 721:</label>\n" +
+            "            <input type=\"number\" id=\"score3\" name=\"score3\" required>\n" +
+            "            <label for=\"unit3\">Unit:</label>\n" +
+            "            <input type=\"number\" id=\"unit3\" name=\"unit3\" value=\"2\" required>\n" +
+            "            <label for=\"score4\">FSC 723:</label>\n" +
+            "            <input type=\"number\" id=\"score4\" name=\"score4\" required>\n" +
+            "            <label for=\"unit4\">Unit:</label>\n" +
+            "            <input type=\"number\" id=\"unit4\" name=\"unit4\" value=\"2\" required>\n" +
+            "            <label for=\"score5\">FSC 731:</label>\n" +
+            "            <input type=\"number\" id=\"score5\" name=\"score5\" required>\n" +
+            "            <label for=\"unit5\">Unit:</label>\n" +
+            "            <input type=\"number\" id=\"unit5\" name=\"unit5\" value=\"2\" required>\n" +
+            "            <label for=\"score6\">FSC 741:</label>\n" +
+            "            <input type=\"number\" id=\"score6\" name=\"score6\" required>\n" +
+            "            <label for=\"unit6\">Unit:</label>\n" +
+            "            <input type=\"number\" id=\"unit6\" name=\"unit6\" value=\"2\" required>\n" +
+            "            <label for=\"score7\">FSC 755:</label>\n" +
+            "            <input type=\"number\" id=\"score7\" name=\"score7\" required>\n" +
+            "            <label for=\"unit7\">Unit:</label>\n" +
+            "            <input type=\"number\" id=\"unit7\" name=\"unit7\" value=\"2\" required>\n" +
+            "            <button type=\"submit\">Calculate CGPA</button>\n" +
+            "        </form>\n" +
+            "    </div>\n" +
+            "</body>\n" +
+            "</html>";
+}
 
-    // Render the HTML form
-    private String renderForm() {
-        return "<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                "    <link rel=\"stylesheet\" href=\"/path/to/style.css\">\n" +
-                "    <title>CGPA Calculator</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "    <div class=\"input-container\">\n" +
-                "        <h1>Enter Your Scores and Units</h1>\n" +
-                "        <form action=\"/\" method=\"get\">\n" +
-                "            <label for=\"score1\">FSC 701:</label>\n" +
-                "            <input type=\"number\" id=\"score1\" name=\"score1\" required>\n" +
-                "            <label for=\"unit1\">Unit:</label>\n" +
-                "            <input type=\"number\" id=\"unit1\" name=\"unit1\" required>\n" +
-                "            <label for=\"score2\">FSC 715:</label>\n" +
-                "            <input type=\"number\" id=\"score2\" name=\"score2\" required>\n" +
-                "            <label for=\"unit2\">Unit:</label>\n" +
-                "            <input type=\"number\" id=\"unit2\" name=\"unit2\" required>\n" +
-                "            <label for=\"score3\">FSC 721:</label>\n" +
-                "            <input type=\"number\" id=\"score3\" name=\"score3\" required>\n" +
-                "            <label for=\"unit3\">Unit:</label>\n" +
-                "            <input type=\"number\" id=\"unit3\" name=\"unit3\" required>\n" +
-                "            <label for=\"score4\">FSC 723:</label>\n" +
-                "            <input type=\"number\" id=\"score4\" name=\"score4\" required>\n" +
-                "            <label for=\"unit4\">Unit:</label>\n" +
-                "            <input type=\"number\" id=\"unit4\" name=\"unit4\" required>\n" +
-                "            <label for=\"score5\">FSC 731:</label>\n" +
-                "            <input type=\"number\" id=\"score5\" name=\"score5\" required>\n" +
-                "            <label for=\"unit5\">Unit:</label>\n" +
-                "            <input type=\"number\" id=\"unit5\" name=\"unit5\" required>\n" +
-                "            <label for=\"score6\">FSC 741:</label>\n" +
-                "            <input type=\"number\" id=\"score6\" name=\"score6\" required>\n" +
-                "            <label for=\"unit6\">Unit:</label>\n" +
-                "            <input type=\"number\" id=\"unit6\" name=\"unit6\" required>\n" +
-                "            <label for=\"score7\">FSC 755:</label>\n" +
-                "            <input type=\"number\" id=\"score7\" name=\"score7\" required>\n" +
-                "            <label for=\"unit7\">Unit:</label>\n" +
-                "            <input type=\"number\" id=\"unit7\" name=\"unit7\" required>\n" +
-
-                "            <button type=\"submit\">Calculate CGPA</button>\n" +
-                "        </form>\n" +
-                "    </div>\n" +
-                "</body>\n" +
-                "</html>";
-    }
 
     // Convert score to points
     private double convertToPoints(int score) {
